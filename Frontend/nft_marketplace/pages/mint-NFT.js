b0f40f63e65b15f856902f17f6dcf73985b48fe3
vishwasp3000 <vishwas_p@ch.iitr.ac.in>
2023-04-22 01:30:06 +0530

    upload metadata to ipfs

--- Frontend/nft_marketplace/pages/mint-NFT.js
+++ Frontend/nft_marketplace/pages/mint-NFT.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
-import { Button, Upload } from "web3uikit"
+import { Button, Upload, Input } from "web3uikit"
+import axios from "axios"
 
 const metadataTemplate = {
     name: "",
@@ -15,13 +16,25 @@ const metadataTemplate = {
 
 export default function MintNFT() {
     const [image, setImage] = useState(null)
+    const [nftName, setNftName] = useState("")
+    const [nftDescription, setNftDescription] = useState("")
+    const [nftPrice, setNftPrice] = useState("")
+
+    const rename = (name) => {
+        let temp = name
+        temp = temp.split(".")
+        temp[temp.length - 1] = ""
+        return temp[0]
+    }
 
     const pinFileToIPFS = async (file) => {
+        let data
+
         const formData = new FormData()
-        formData.append("file", file.name)
+        formData.append("file", file)
 
         const metadata = JSON.stringify({
-            name: "File name",
+            name: rename(file.name),
         })
         formData.append("pinataMetadata", metadata)
 
@@ -40,7 +53,7 @@ export default function MintNFT() {
                 body: formData,
             })
 
-            const data = await response.json()
+            data = await response.json()
             console.log(data)
             // Response example:
             // {
@@ -51,8 +64,65 @@ export default function MintNFT() {
         } catch (error) {
             console.log(error)
         }
-
         return data
+    }
+
+    const handleTokenUri = async () => {
+        let tokenUriMetadata = { ...metadataTemplate }
+
+        // "pinataOptions": {
+        //     "cidVersion": 1
+        //   },
+        //   "pinataMetadata": {
+        //     "name": "testing",
+        //     "keyvalues": {
+        //       "customKey": "customValue",
+        //       "customKey2": "customValue2"
+        //     }
+        //   },
+        //   "pinataContent": {
+        //     "somekey": "somevalue"
+        //   }
+
+        const ipfsImageData = await pinFileToIPFS(image)
+
+        tokenUriMetadata.name = nftName
+        tokenUriMetadata.description = nftDescription
+        tokenUriMetadata.image = `ipfs://${ipfsImageData.IpfsHash}`
+
+        let metadataToUpload = {
+            pinataOptions: {
+                cidVersion: 1,
+            },
+
+            pinataMetadata: {
+                name: nftName,
+            },
+            pinataContent: {
+                tokenUriMetadata,
+            },
+        }
+
+        console.log("metadata: ", metadataToUpload)
+
+        console.log(`Uploading ${tokenUriMetadata.name} metadata to IPFS`)
+
+        const config = {
+            method: "post",
+            url: "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+            headers: {
+                "Content-Type": "application/json",
+                pinata_api_key: process.env.NEXT_PUBLIC_PINATA_API_KEY,
+                pinata_secret_api_key: process.env.NEXT_PUBLIC_PINATA_API_SECRET,
+            },
+            data: metadataToUpload,
+        }
+        try {
+            const response = await axios(config)
+            console.log(response.data)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return (
@@ -73,7 +143,31 @@ export default function MintNFT() {
                 onClick={function noRefCheck() {
                     pinFileToIPFS(image)
                 }}
-                text="Colored Button: Green"
+                text="Upload Image"
+                theme="colored"
+            />
+            <Input
+                // description="Enter name of the NFT"
+                label="NFT Name"
+                name="Test text Input"
+                onChange={function noRefCheck(event) {
+                    setNftName(event.target.value)
+                }}
+            />
+            <Input
+                // description="Enter description of the NFT"
+                label="NFT Description"
+                name="Test text Input"
+                onChange={function noRefCheck(event) {
+                    setNftDescription(event.target.value)
+                }}
+            />
+            <Button
+                color="green"
+                onClick={function noRefCheck() {
+                    handleTokenUri()
+                }}
+                text="Upload Metadata"
                 theme="colored"
             />
         </div>
